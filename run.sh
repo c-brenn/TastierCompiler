fa95ce8e0e002ac3e20ccfb25613f89ef8aefca3
c-brenn <brennc27@tcd.ie>
2016-01-03 12:51:28 +0000

    Constants
    Added constants and changed how procedure declarations work - they are
    now wrapped with a label like nested procedures are so you can't fall in
    to a procedure, they must be explicitly called.
    This allows constant and variables declared globally to be used anywhere
    in the program.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ if make; then
   pushd ../machine
   if cabal install; then
     popd
-    mono bin/tcc.exe test/Programs/$1 test.asm
+    mono bin/tcc.exe test/Programs/$1.tas test.asm
     if [ -e test.asm ]; then
       if tasm test.asm test.bc; then
         tvm test.bc test/Inputs/test.IN
